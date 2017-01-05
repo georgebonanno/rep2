1bde656170cf8f495949389da904995eb52d4c12
georgebonanno <george.bonanno@gmail.com>
2017-01-05 11:55:18 +0100

    loading csv

--- question2/csvProcessing.R
+++ question2/csvProcessing.R
@@ -1,4 +1,34 @@
 propertyDetails <- read.csv("unique_features.csv",
-                            header = TRUE,sep = ",")
+                            header = TRUE,sep = ",",
+                            fileEncoding = "UTF-8",
+                            na.strings = c(""),
+                            row.names = NULL)
 
-propertyDetails$price_euro <- as.numeric(propertyDetails$price_euro)
+
+head(propertyDetails[is.na(propertyDetails$location),],30)
+
+#propertyDetails$price_euro <- as.numeric(propertyDetails$price_euro)
+
+naCount <- function(...) {
+  params <- list(...)
+  markNas <- function(prev,x) {
+    if (length(x) > 1) {
+      print(paste("warning: ",x))
+    }
+    
+    if (is.na(x)) {
+      n=1
+    } else {
+      n=0
+    }
+    
+    return(n+prev)
+  }
+  Reduce(markNas,params,init=0)
+}
+
+oneUnknown <- propertyDetails[ifelse(is.na(propertyDetails$location),1,0)+
+                  ifelse(is.na(propertyDetails$price_euro),1,0)+
+                  ifelse(is.na(propertyDetails$property_type),1,0)+
+                  ifelse(is.na(propertyDetails$area_sqm),1,0)
+                  <= 1,]
