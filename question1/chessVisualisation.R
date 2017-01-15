67450b57cbc64117209c32dc9a239f89659e55c1
georgebonanno <george.bonanno@gmail.com>
2017-01-15 09:45:22 +0100

    games time series

--- question1/chessVisualisation.R
+++ question1/chessVisualisation.R
@@ -20,6 +20,9 @@ resultCount <- '
 
 numberOfMovesCount <- 'select move_count from games'
 
+yearlyGamesQuery <- 'select substr(g.date_of_game,1,4) game_yr,count(*) number_of_games from games g 
+  	                 group by substr(g.date_of_game,1,4)'
+
 retrievefirstWinnerMoveCount <- function() {
   #initialise con to NA to allow finally 
   #block to close con only if a connection
@@ -30,10 +33,12 @@ retrievefirstWinnerMoveCount <- function() {
     firstWinnerMoveCount <- executeAndGetAllRows(con,firstWinnerMoveQuery)
     resultCount <- executeAndGetAllRows(con,resultCount)
     moveCount <- executeAndGetAllRows(con,numberOfMovesCount)
+    yearlyGames <- executeAndGetAllRows(con,yearlyGamesQuery)
     
     return (list(firstWinnerMoveCount=firstWinnerMoveCount,
                  resultCount=resultCount,
-                 moveCount=moveCount))
+                 moveCount=moveCount,
+                 yearlyGames=yearlyGames))
   },finally = {
     if (!is.na(con)) {
       dbDisconnect(con)
@@ -56,4 +61,13 @@ moveInGameBoxPlot <- function() {
 
 winningMovesHeatMap <- function() {
   ggplot(data=firstWinnerCnt,aes(x=col,y=rw))+geom_tile(aes(fill=move_count))
+}
+
+plotYearlyGames <- function() {
+  yGames <- chessStats$yearlyGames
+  yGames$yearDate <- as.Date(yGames$game_yr,"%Y")
+  ggplot(yGames, aes(x=yearDate, y=number_of_games)) + geom_line() + theme_classic() +
+     xlab("year") + ylab("Games Per Year") + 
+    ggtitle("A Time series of yearly game count") +
+    
 }
